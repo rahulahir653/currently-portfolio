1f1b9174ca7c0a283e7940d271e0f542cf338cba
Rahul Chavda <75974091+rahulahir653@users.noreply.github.com>
2023-09-23 19:12:19 +0530

    Add files via upload

---
+++ Untitled-1.sh
@@ -0,0 +1,14 @@
+<!-- FAVICON -->
+    <link rel="icon" type="image/png" href="images/favicon.png">
+
+    <!-- font awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Bootstrap 5 CDN Links -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- Custom File's Link -->
+    <link rel="stylesheet" href="C:\Users\shyam\Documents\current_portfolio-main\current_portfolio-main\bootstap portfolio\css\style.css">
+    <link rel="stylesheet" href="css/responsive-style.css">
+    <script src="gsp.js/greenshock.js"></script>
